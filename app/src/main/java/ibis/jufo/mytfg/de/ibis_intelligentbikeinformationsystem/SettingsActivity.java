2f5fc3e0f3d72895e5c1270dbfd2e4ed971c7fd5
Leonard Rapp <leonardrapp@gmx.de>
2014-12-26 13:09:51 +0100

    added ButtonOpenSettings below welcome text

--- app/src/main/java/ibis/jufo/mytfg/de/ibis_intelligentbikeinformationsystem/SettingsActivity.java
+++ app/src/main/java/ibis/jufo/mytfg/de/ibis_intelligentbikeinformationsystem/SettingsActivity.java
@@ -1,32 +1,24 @@
 package ibis.jufo.mytfg.de.ibis_intelligentbikeinformationsystem;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
 
-
-    public void openSettings(View view) {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
-
+public class SettingsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
 
