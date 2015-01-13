9c88486a1c96e2ca34de9c6b58ebad3f6d3a3676
Leonard Rapp <leonardrapp@gmx.de>
2015-01-13 22:38:08 +0100

    MapFragment: added some Overlays

--- app/src/main/java/ibis/jufo/mytfg/de/ibis_intelligentbikeinformationsystem/MapFragment.java
+++ app/src/main/java/ibis/jufo/mytfg/de/ibis_intelligentbikeinformationsystem/MapFragment.java
@@ -1,15 +1,69 @@
 package ibis.jufo.mytfg.de.ibis_intelligentbikeinformationsystem;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.util.ResourceProxyImpl;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
 public class MapFragment extends Fragment {
+
+    private MapView mMapView;
+    private ResourceProxy mResourceProxy;
+    private MyLocationNewOverlay mLocationOverlay;
+    private CompassOverlay mCompassOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
+        mMapView = new MapView(inflater.getContext(), 256, mResourceProxy);
+        return mMapView;
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        final Context context = this.getActivity();
+        final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+
+        //set zoom and touch controls
+        mMapView.setBuiltInZoomControls(true);
+        mMapView.setMultiTouchControls(true);
+        //crate and enable CompassOverlay
+        this.mCompassOverlay = new CompassOverlay(context, new InternalCompassOrientationProvider(context), mMapView);
+        mCompassOverlay.enableCompass();
+        //crate and enable MyLocationOverlay
+        this.mLocationOverlay = new MyLocationNewOverlay(context, new GpsMyLocationProvider(context),mMapView);
+        mLocationOverlay.enableMyLocation();
+        //crate and enable ScaleBarOverlay
+        mScaleBarOverlay = new ScaleBarOverlay(context);
+        mScaleBarOverlay.setCentred(true);
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        //add overlays
+        mMapView.getOverlays().add(this.mLocationOverlay);
+        mMapView.getOverlays().add(this.mCompassOverlay);
+        mMapView.getOverlays().add(this.mScaleBarOverlay);
+        mMapView.getController().setZoom(15);
+        //center at pseudo position.
+        //TODO: center at users position
+        GeoPoint pseudoLocation = new GeoPoint(51.3056777, 6.7461651);
+        mMapView.getController().setCenter(pseudoLocation);
+
     }
 }
