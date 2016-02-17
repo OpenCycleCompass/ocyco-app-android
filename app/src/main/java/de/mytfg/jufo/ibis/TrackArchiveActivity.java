72381bd0acf5cab0c118482d4b88f7421cb71953
Raphael Lehmann <raphael@rleh.de>
2016-02-17 02:22:04 +0100

    TrackArchiveActivity: access tracks through global IbisTrackArchive instance; upload track only if it is not uploaded yet

--- app/src/main/java/de/mytfg/jufo/ibis/TrackArchiveActivity.java
+++ app/src/main/java/de/mytfg/jufo/ibis/TrackArchiveActivity.java
@@ -1,8 +1,6 @@
 package de.mytfg.jufo.ibis;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,9 +9,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 import de.mytfg.jufo.ibis.storage.IbisTrack;
@@ -25,33 +20,36 @@ import de.mytfg.jufo.ibis.util.Utils;
  * or click on the delete all button to delete any archived track
  */
 public class TrackArchiveActivity extends AppCompatActivity {
-    private long[] track_names = new long[0];
     private ArrayList<String> arrayList = new ArrayList<>();
     private ArrayAdapter<String> adapter;
-    private SharedPreferences sharedPrefs;
-    private ArrayList<IbisTrack> tracks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_archive);
-        ListView listView = (ListView) findViewById(R.id.trackListView);
-        tracks = new ArrayList<>();
-        sharedPrefs = getSharedPreferences(
-                getString(R.string.preference_file_key_upload_later), Context.MODE_PRIVATE);
         adapter = new ArrayAdapter<>(
                 this, android.R.layout.simple_list_item_1, arrayList);
+        ListView listView = (ListView) findViewById(R.id.trackListView);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(),
-                        "Upload Track " + track_names[position], Toast.LENGTH_LONG)
-                        .show();
-                Intent intent = new Intent(getApplicationContext(), UploadTrackActivity.class);
-                intent.putExtra("track", track_names[position]);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                if (!IbisApplication.trackArchive.getTrackMetadataList().get(position).
+                        isUploaded()) {
+                    Toast.makeText(getApplicationContext(), "Upload Track ...",
+                            Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(), UploadTrackActivity.class);
+                    intent.putExtra("track", IbisApplication.trackArchive.getTrackMetadataMap().get(
+                            IbisApplication.trackArchive.getTrackUuidList().get(position)
+                    ).getUuid().toString());
+                    intent.putExtra("fromArchive", true);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), R.string.upload_track_already_uploaded,
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
         updateUI();
@@ -62,32 +60,11 @@ public class TrackArchiveActivity extends AppCompatActivity {
       */
     private void updateUI() {
         arrayList.clear();
-        if (sharedPrefs.contains("trackNameList")) {
-            // read list of archived tracks from shard preferences
-            String trackNameList = sharedPrefs.getString("trackNameList", "");
-            String[] track_names_string = trackNameList.split(";");
-            // allocate/clear lists for track ids and track database objects
-            track_names = new long[track_names_string.length];
-            tracks.clear();
-            tracks.ensureCapacity(track_names_string.length);
-            for (int i = 0; i < track_names_string.length; i++) {
-                track_names[i] = Long.parseLong(track_names_string[i]);
-                ObjectInputStream input;
-                String filename = track_names_string[i] + ".track";
-                try {
-                    input = new ObjectInputStream(new FileInputStream(new File(new File(getFilesDir(),"")+ File.separator+filename)));
-                    IbisTrack track = (IbisTrack) input.readObject();
-                    input.close();
-                    tracks.add(track);
-                    arrayList.add(Utils.getDateTime(track.metaData.getStartTime())
-                            + " (" + getString(R.string.upload_track_duration) + ": "
-                            + Utils.formatTime(track.metaData.getDuration()) + ")");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, R.string.track_archive_corrupt, Toast.LENGTH_LONG).show();
-                }
-            }
+
+        for (IbisTrack.MetaData metadata : IbisApplication.trackArchive.getTrackMetadataList()) {
+            arrayList.add(Utils.getDateTime(metadata.getStartTime()));
         }
+
         adapter.notifyDataSetChanged();
     }
 
@@ -95,10 +72,7 @@ public class TrackArchiveActivity extends AppCompatActivity {
      * onClick method for TrackArchiveDelete button in activity_track_archive.xml
      * @param view view
      */
-    public void onClickTrackArchiveDelete(View view) {
-        SharedPreferences.Editor sharedPrefsEditor = sharedPrefs.edit();
-        sharedPrefsEditor.clear();
-        sharedPrefsEditor.apply();
-        updateUI();
+    public void onClickTrackArchiveDeleteAll(View view) {
+        IbisApplication.trackArchive.deleteAll();
     }
 }
