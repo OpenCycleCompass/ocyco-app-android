0406e37220be1aaf7f2203e8359037da939c5586
Raphael Lehmann <raphael@rleh.de>
2016-04-01 19:58:47 +0200

    travis ci: add deploy to https://dl.ocyco.de/

---
+++ upload.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# abort on error
+set -e
+set -u
+
+BASE_URL="https://dl.ocyco.de/"
+FILE=$(find . | grep -P 'app/build/outputs/apk/ocyco-app-.+\.apk')
+FILE_NAME=$(echo ${FILE} | rev | cut -d/ -f1 | rev)
+
+curl --verbose --upload-file ${FILE} --user ${OCYCO_UPLOAD_AUTH} ${BASE_URL}branches/${TRAVIS_BRANCH_ESCAPED}/${FILE_NAME}
+
+if [ "$TRAVIS_BRANCH" = "develop" ]
+then
+    echo "Branch is 'develop': update $BASE_URL/ocyco-develop.apk (updating redirect)"
+    curl --verbose -X PUT -H 'Content-Type: text/text' -d "branches/${TRAVIS_BRANCH_ESCAPED}/${FILE_NAME}" --user ${OCYCO_UPLOAD_AUTH} https://dl.ocyco.de/branches/current-develop
+fi
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
+    echo "Branch is 'master': update $BASE_URL/ocyco-master.apk (updating redirect)"
+    curl --verbose -X PUT -H 'Content-Type: text/text' -d "branches/${TRAVIS_BRANCH_ESCAPED}/${FILE_NAME}" --user ${OCYCO_UPLOAD_AUTH} https://dl.ocyco.de/branches/current-master
+fi
+
+exit 0
